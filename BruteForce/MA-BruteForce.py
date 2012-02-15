6d6cf3fb013b02f19e85fe855649da5d4a058aa8
Mads Anthon <madsanthon@gmail.com>
2012-02-15 14:00:18 +0100

    Updated version

--- BruteForce/MA-BruteForce.py
+++ BruteForce/MA-BruteForce.py
@@ -30,8 +30,8 @@ while t < T: #setting time of run
     #initializing data
     xi = data[:,:3]
     vi = data[:,3:6]
-    inv_r = []
-    a = [0,0,0]
+    inv_r = 0
+    a = np.zeros(3)
 
     #calculating accereration
     #ma = (Gm/(r*r))*|r| -> a = G*|r|/(r*r)
@@ -40,10 +40,10 @@ while t < T: #setting time of run
       r = xi[j]-xi[i]
       if np.linalg.norm(r) > 0:
         inv_r = 1.0/np.linalg.norm(r) #invers distance from xi[j] to xi[j]
-    a = G*(inv_r*inv_r)*r
-
+        a += G*(inv_r*inv_r)*r
+    
     #updating data for next loop
-    data[:,:3] = xi + vi*dt + 0.5*a*dt*dt
+    data[:,:3] = xi + vi*dt
     data[:,3:6] = vi + a*dt
 
   t += dt #counter
