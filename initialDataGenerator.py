730424b17a9da7ed01efa3bed10359551bc8b0c7
Mossa Merhi <cgmossa@gmail.com>
2012-02-22 15:13:09 +0100

    Changed range of positions and set velocities to zero

--- initialDataGenerator.py
+++ initialDataGenerator.py
@@ -9,15 +9,34 @@ __credits__     = ["Mads Anthon", "Mossa Merhi", "Orhan Toy"]
 __version__     = "0.7"
 
 import numpy as np
+from numpy import linalg as la
 import sys
 
-N = 10 # Default
+N = 1000 # Default
+epsilon = 10**(-6)
 
 if len(sys.argv) > 1 and int(sys.argv[1]) > 0:
     N = int(sys.argv[1])
 
 # Generate a N x 6 matrix and save it
-data = np.random.rand(N, 6)
+pos = np.random.rand(N,3) - (1./2) * np.ones((N,3))
+vel = np.zeros((N,3))
+m = np.random.rand(N,1)
+m = m/la.norm(m,1);
+#
+# Test!
+#
+print "All positioner er i [-0.5,0.5]",
+if filter(lambda a: 0.5-abs(a) < epsilon, pos.ravel()) == []:
+    print "Ja"
+else:
+    print "Nej!"
+print "Måske?"
+print "Summen af alle partiklers hastigheder er", np.sum(vel)
+print "Sum af alle masser",np.sum(m)
+
+data = np.hstack((pos,vel,m))
+
 np.savetxt('initialData-N' + str(N) + '.txt', data)
 
 print 'Data file with', N, 'particles has been generated.'
