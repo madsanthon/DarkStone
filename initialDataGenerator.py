fe66ed45434a4f6131f98708211b43fad85e52a0
Orhan Toy <ot@peytz.dk>
2012-03-05 03:54:06 +0100

    Cleaned up generator

--- initialDataGenerator.py
+++ initialDataGenerator.py
@@ -5,38 +5,26 @@
 positions and velocities. The data is saved into a file called 
 initialData-N<N>.txt, where <N> is the number of particles generated."""
 
-__author__      = "Orhan Toy"
+__author__      = "Mossa Merhi"
 __credits__     = ["Mads Anthon", "Mossa Merhi", "Orhan Toy"]
 __version__     = "0.7"
 
 import numpy as np
-from numpy import linalg as la
 import sys
 
 N = 1000 # Default
-epsilon = 10**(-6)
 
 if len(sys.argv) > 1 and int(sys.argv[1]) > 0:
     N = int(sys.argv[1])
 
-# Generate a N x 6 matrix and save it
-pos = np.random.rand(N,3) - (1./2) * np.ones((N,3))
-vel = np.zeros((N,3))
-m = np.random.rand(N,1)
-m = m/la.norm(m,1);
-#
-# Test!
-#
-print "All positioner er i [-0.5,0.5]",
-if filter(lambda a: 0.5-abs(a) < epsilon, pos.ravel()) == []:
-    print "Ja"
-else:
-    print "Nej!"
-print "Måske?"
-print "Summen af alle partiklers hastigheder er", np.sum(vel)
-print "Sum af alle masser",np.sum(m)
+# Generate N random positions in the interval -0.5..0.5 for x, y and z
+pos = np.random.rand(N, 3) - 0.5*np.ones((N, 3))
 
-data = np.hstack((pos,vel,m))
+# Generate N random masses, summing to 1
+m = np.random.rand(N, 1)
+m = m/np.linalg.norm(m, 1) # (Sum) normalization
+
+data = np.hstack((pos, m))
 
 np.savetxt('initialData-N' + str(N) + '.txt', data)
 
