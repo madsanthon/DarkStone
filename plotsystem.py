66135e390daad910d6e039f60a39e10e6d39ac62
Orhan Toy <ot@peytz.dk>
2012-03-08 01:05:09 +0100

    Cleaning up again

--- plotsystem.py
+++ plotsystem.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 import numpy as np
-from Bins import *
+from bins import *
 
 class PlotSystem(object):
     def __init__(self):
